6c3adedd6fd40ebac001a16d3e06d97aec98bf79
Mika Jauhonen <me@mika.xyz>
2016-12-04 23:26:21 +0100

    Add sandbox app

--- src/Util/Debug.elm
+++ src/Util/Debug.elm
@@ -1,6 +1,20 @@
 module Util.Debug exposing (..)
 
 
+debugV : String -> v -> a -> a
+debugV m v a =
+    let
+        _ =
+            Debug.log m v
+    in
+        a
+
+
+debugOffV : String -> v -> a -> a
+debugOffV m v a =
+    a
+
+
 debugOn : String -> a -> a
 debugOn m a =
     Debug.log m a
